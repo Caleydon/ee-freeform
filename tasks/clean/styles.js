01e19f4c1febf023aa00e5f030905738c776938d
Sean Delaney <sean@solspace.com>
2025-08-11 21:21:04 +0100

    fix(SFT-1177): updated Flatpickr JS and CSS to no longer be inline, resolving conflict issues with other addons/scripts (#198)
    Co-authored-by: kjmartens <kelsey@solspace.com>

--- tasks/clean/styles.js
+++ tasks/clean/styles.js
@@ -3,6 +3,10 @@ const paths = require("../_paths"),
 
 module.exports = {
   fn: function (gulp, callback) {
-    return del(paths.styles.dest);
+    return del([
+      paths.styles.dest + "**/*",                           // delete all files
+      "!" + paths.styles.dest + "fields/datepicker.css",    // exclude datepicker files
+      "!" + paths.styles.dest + "fields",                   // exclude the folder itself
+    ]);
   },
 };
